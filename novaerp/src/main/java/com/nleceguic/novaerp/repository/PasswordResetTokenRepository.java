117e157934b70c31c95661c85edef222b0b18bc4
nleceguic <nleceguic@gmail.com>
2025-10-20 20:11:42 +0200

    Rename findByToken to findByTokenId in repository
    The method in PasswordResetTokenRepository was renamed from findByToken to findByTokenId to better reflect the parameter's purpose and improve code clarity.

--- novaerp/src/main/java/com/nleceguic/novaerp/repository/PasswordResetTokenRepository.java
+++ novaerp/src/main/java/com/nleceguic/novaerp/repository/PasswordResetTokenRepository.java
@@ -2,9 +2,10 @@ package com.nleceguic.novaerp.repository;
 
 import com.nleceguic.novaerp.entity.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
-    Optional<PasswordResetToken> findByToken(String token);
+    Optional<PasswordResetToken> findByTokenId(String tokenId);
     void deleteAllByUserId(Long userId);
 }
